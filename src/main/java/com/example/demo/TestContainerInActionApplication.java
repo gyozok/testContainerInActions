1b239f4a66e6c0dd98f909351e4a22604f74860b
GyozoCliq <g.karer@cliqdigital.com>
2023-11-10 11:37:25 +0100

    basic project without test

---
+++ src/main/java/com/example/demo/TestContainerInActionApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestContainerInActionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestContainerInActionApplication.class, args);
+	}
+
+}
